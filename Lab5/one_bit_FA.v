aea3b9b86cf8862e0c7fa70931a51b5edd861929
clink0 <97566649+clink0@users.noreply.github.com>
2026-02-10 15:28:35 -0700

    newnew

--- Lab5/one_bit_FA.v
+++ Lab5/one_bit_FA.v
@@ -1,9 +1,15 @@
-module one_bit_FA(s, co, x, y, ci);
+module one_bit_FA (
+    s,
+    co,
+    x,
+    y,
+    ci
+);
 
-input x, y, ci;
-output s, co;
+  input x, y, ci;
+  output s, co;
 
-assign co = (x&y) | (ci&(x^y));
-assign s = x^y^ci;
+  assign co = (x & y) | (ci & (x ^ y));
+  assign s  = x ^ y ^ ci;
 
 endmodule
--- Lab5/twos_comp_adder.v
+++ Lab5/twos_comp_adder.v
@@ -1,22 +1,46 @@
-module twos_comp_adder(S, OV, A, B, sub);
+module twos_comp_adder (
+    S,
+    OV,
+    A,
+    B,
+    sub
+);
 
-input [2:0] A, B;
-input sub;
+  input [2:0] A, B;
+  input sub;
 
-output [2:0] S;
-output OV;
+  output [2:0] S;
+  output OV;
 
-wire [2:0] B_xor;
-assign B_xor[0] = B[0] ^ sub;
-assign B_xor[1] = B[1] ^sub;
-assign B_xor[1] = B[1] ^sub;
+  wire [2:0] B_xor;
+  assign B_xor[0] = B[0] ^ sub;
+  assign B_xor[1] = B[1] ^ sub;
+  assign B_xor[1] = B[1] ^ sub;
 
-wire C0, C1, C2;
+  wire C0, C1, C2;
 
-one_bit_FA FA0(.s(S[0]), .co(C0), .x(A[0]), .y(B_xor[0]), .ci(sub));
-one_bit_FA FA1(.s(S[1]), .co(C1), .x(A[1]), .y(B_xor[1]), .ci(C0));
-one_bit_FA FA2(.s(S[2]), .co(C2), .x(A[2]), .y(B_xor[2]), .ci(C1));
+  one_bit_FA FA0 (
+      .s (S[0]),
+      .co(C0),
+      .x (A[0]),
+      .y (B_xor[0]),
+      .ci(sub)
+  );
+  one_bit_FA FA1 (
+      .s (S[1]),
+      .co(C1),
+      .x (A[1]),
+      .y (B_xor[1]),
+      .ci(C0)
+  );
+  one_bit_FA FA2 (
+      .s (S[2]),
+      .co(C2),
+      .x (A[2]),
+      .y (B_xor[2]),
+      .ci(C1)
+  );
 
-assign AV = C1 ^ C2;
+  assign AV = C1 ^ C2;
 
 endmodule
